2d56a442ab9ffdcc4ef0ab4e8bdf123414063eec
D-Sketon <2055272094@qq.com>
2024-11-29 22:24:55 +0800

    Site updated: 2024-11-29 22:24:53

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732889259459";
+const VERSION = "1732890286650";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
